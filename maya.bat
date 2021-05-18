9c76072669910eb5b414cda383af58dd4817cbdd
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2021-05-19 00:17:55 +0900

    maya起動バッチから日本語コメントを除外

--- maya.bat
+++ maya.bat
@@ -1,30 +1,30 @@
 @echo off
 
-REM 遅延環境変数有効
+REM Enable lazy environment variable setting.
 setlocal EnableDelayedExpansion
 
-@REM .pycファイルを生成しない
+@REM Prevent the generation of .pyc files
 Set PYTHONDONTWRITEBYTECODE=1
 
-@REM MAYA_MODULE_PATHにカレントディレクトリを追加
+@REM Add the current directory to MAYA_MODULE_PATH.
 cd /d %~dp0\..\
 Set MAYA_MODULE_PATH=%cd%;%MAYA_MODULE_PATH%
 
-@REM 引数を取得
+@REM Get the arguments.
 Set LANGUAGE=%1
 Set MAYA_VER=%2
 
-@REM ------- Mayaインストールフォルダを検索 -------------------------
+@REM ------- Find the Maya installation folder -------------------------
 Set MAYA_APP_PATH=null
 Set MAYA_MAX_VER=2020
 Set MAYA_MIN_VER=2015
 
-@REM バージョン指定があった場合検索上限を指定バージョンにする
+@REM If MAYA_VER is specified, set the search limit to the specified version.
 if not "!MAYA_VER!"=="" (
     Set MAYA_MAX_VER=%MAYA_VER%
 )
 
-@REM レジストリから全Mayaインストールフォルダを検索して見つかったら実行処理へ移動
+@REM Search the registry for all Maya installation folders, and if found, move to the execution process.
 for /l %%v in (%MAYA_MAX_VER%, -1, %MAYA_MIN_VER%) do (
     FOR /F "TOKENS=1,2,*" %%I IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Autodesk\Maya\%%v\Setup\InstallPath" /v "MAYA_INSTALL_LOCATION"') DO IF "%%I"=="MAYA_INSTALL_LOCATION" SET MAYA_APP_PATH=%%K
 
@@ -32,16 +32,16 @@ for /l %%v in (%MAYA_MAX_VER%, -1, %MAYA_MIN_VER%) do (
 )
 goto except
 
-@REM ------- Maya実行 -------------------------
+@REM ------- Execute Maya -------------------------
 :execute
-@REM 言語設定(en_US or ja_JP)
+@REM Language setting(en_US or ja_JP)
 Set MAYA_UI_LANGUAGE=%LANGUAGE%
 
-@REM Maya起動
+@REM Execute Maya
 start "" "%MAYA_APP_PATH%\bin\maya.exe"
 goto end
 
-@REM ------- インストールされてない場合の例外処理 -------------------------
+@REM ------- Exception handling when not installed. -------------------------
 :except
 echo Maya is not installed.
 pause
